346cf680258909acce23866364c0e4afe331d96a
wthomas0187 <wthomas0187@gmail.com>
2016-05-11 12:18:30 -0400

    Add files via upload

---
+++ breaktime.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime() )
+
+while(break_count < total_breaks):
+    time.sleep(7200)
+    webbrowser.open("https://www.youtube.com/watch?v=vy4sEnQtZVw")
+    break_count = break_count + 1
